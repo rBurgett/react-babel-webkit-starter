3477f9b22fad6c2857874548e333d22853e6b69a
Ryan <ryan@burgettweb.net>
2015-12-21 16:50:57 -0500

    Added babelrc

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,9 @@ import ReactDOM from 'react-dom';
 
 let hello = (name: string): string => {
 
-<<<<<<< HEAD
-=======
-let hello = function(name) {
-
->>>>>>> cbc7ec508bd952d55fc92db5d607f09b5decf13e
     return `Hello, ${name}!`;
 
-}
+};
 
 class MyComponent extends React.Component {
 
@@ -24,8 +19,4 @@ class MyComponent extends React.Component {
 
 }
 
-<<<<<<< HEAD
 ReactDOM.render(<MyComponent />, document.getElementById('js-container'));
-=======
-React.render(<MyComponent />, document.getElementById('js-container'));
->>>>>>> cbc7ec508bd952d55fc92db5d607f09b5decf13e
