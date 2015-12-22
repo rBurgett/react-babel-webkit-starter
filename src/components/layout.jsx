434377978c4f0cf6cd1559fbd8257a58746f2f62
Ryan <ryan@burgettweb.net>
2015-12-22 14:20:45 -0500

    Configured client-only routes

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,13 +7,22 @@ class Layout extends React.Component {
 
     render() {
 
-        console.log(this.props);
+        const styles = {
+            button: {
+                margin: 5
+            }
+        };
+
+        const currentPath = this.props.location.pathname;
+
+        const activeClasses = 'btn btn-primary';
+        const inactiveClasses = 'btn btn-default';
 
         return (
             <div>
                 <h1>This is the layout component!</h1>
-                <Link to="/"><button type="button" className="btn btn-default">Home</button></Link>
-                <Link to="/about"><button type="button" className="btn btn-default">About</button></Link>
+                <Link to="/"><button type="button" className={(currentPath === '/') ? activeClasses : inactiveClasses} style={styles.button}>Home</button></Link>
+                <Link to="about"><button type="button" className={(currentPath === '/about') ? activeClasses : inactiveClasses} style={styles.button}>About</button></Link>
                 {this.props.children}
             </div>
         );
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,18 @@
 /* @flow */
 
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute } from 'react-router';
-import { createHistory, useBasename } from 'history';
 
 import About from './components/about/about.jsx';
 import Home from './components/home/home.jsx';
 import Layout from './components/layout.jsx';
 
-const history = useBasename(createHistory)({
-  basename: '/'
-});
-
-render((
-    <Router history={history}>
+ReactDOM.render((
+    <Router>
         <Route path='/' component={Layout}>
-            <IndexRoute name='home' component={Home} />
-            <Route name='about' path='/about' component={About} />
+            <IndexRoute component={Home} />
+            <Route path='/about' component={About} />
         </Route>
     </Router>
 ), document.getElementById('js-container'));
